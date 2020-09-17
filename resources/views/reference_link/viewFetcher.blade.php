55b2b9bf39d7a1661b84aa493596bda2d4528815
X\maste <aiman.hz@protonmail.com>
2020-09-17 22:13:29 +0800

    EDIT | changed form fetcher layouts

--- resources/views/reference_link/viewFetcher.blade.php
+++ resources/views/reference_link/viewFetcher.blade.php
@@ -1,5 +1,4 @@
-@extends('layouts.master')
-
+@extends('layouts.app')
 
 @section('top')
     <!-- DataTables -->
@@ -7,28 +6,57 @@
 @endsection
 
 @section('content')
+<div class="" id="modal-form" tabindex="1" role="dialog" data-backdrop="static" onsubmit="onSubmit()">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form method="POST" action="{{ route('admin.fetch.reference.link', $found->reference_code) }}">
+                @csrf
+                <div class="form-group col-md-12">
+                    <h2>Please fill out the form to continue contacting us.</h2>
+                </div>
+                <div class="form-group col-md-12">
+                    <label >Name</label>
+                    <input type="text" class="form-control" id="nama" name="nama"  autofocus required>
+                    <span class="help-block with-errors"></span>
+                </div>
+                <div class="form-group col-md-12">
+                    <label >Email</label>
+                    <input type="email" class="form-control" id="email" name="email">
+                    <span class="help-block with-errors"></span>
+                </div>
+                <div class="form-group col-md-12">
+                    <label >Phone</label>
+                    <input type="text" class="form-control" id="telepon" name="telepon">
+                    <span class="help-block with-errors"></span>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Send</button>
+                </div>
+            </form>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->    
 
-    <form method="POST" action="{{ route('admin.fetch.reference.link', $found->reference_code) }}">
-        @csrf
-        <div class="form-group">
-            <label >Name</label>
-            <input type="text" class="form-control" id="nama" name="nama"  autofocus required>
-            <span class="help-block with-errors"></span>
-        </div>
-        <div class="form-group">
-            <label >Email</label>
-            <input type="email" class="form-control" id="email" name="email">
-            <span class="help-block with-errors"></span>
-        </div>
-        <div class="form-group">
-            <label >Phone</label>
-            <input type="text" class="form-control" id="telepon" name="telepon">
-            <span class="help-block with-errors"></span>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Save</button>
-        </div>
-    </form>
+@endsection
+
+@section('bot')
+
+    <!-- DataTables -->
+    <script src=" {{ asset('assets/bower_components/datatables.net/js/jquery.dataTables.min.js') }} "></script>
+    <script src="{{ asset('assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }} "></script>
+
+    {{-- Validator --}}
+    <script src="{{ asset('assets/validator/validator.min.js') }}"></script>
+
+    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>--}}
+
+    <script type="text/javascript">
+        function onSubmit() {
+            alert("The form was submitted");
+        }
+    </script>
 
 @endsection
